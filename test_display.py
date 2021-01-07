e5445d41caa302ae047b826553547d717503ed71
Jochen Szaroletta <jochen.szaroletta@linde-le.com>
2021-01-07 17:28:05 +0100

    even better now

--- test_display.py
+++ test_display.py
@@ -9,24 +9,43 @@ from luma.core.interface.serial import spi
 from luma.core.render import canvas
 from luma.oled.device import sh1106
 
-#from .lib_oled96 import ssd1306
+
 from smbus import SMBus
 from lib_oled96.lib_oled96 import ssd1306
 
-# Display einrichten
-i2cbus = SMBus(1)            # 0 = Raspberry Pi 1, 1 = Raspberry Pi > 1
-oled = ssd1306(i2cbus)
 
-# Ein paar Abkürzungen, um den Code zu entschlacken
-draw = oled.canvas
 
-# Display zum Start löschen
-oled.cls()
-oled.display()
+def init_spi_display():
+    serial = spi(port=0, device=1, gpio_DC=27, gpio_RST=24)
+    device = sh1106(serial, rotate=0)
 
-# Hallo Welt
-draw.text((1, 16), "TrainControll 2021", fill=1)
-draw.text((1, 40), "", fill=1)
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((30, 40), "Hello World", fill="white")
 
-# Ausgaben auf Display schreiben
-oled.display()
+
+
+
+def init i2c_display():
+        # Display einrichten
+        i2cbus = SMBus(1)            # 0 = Raspberry Pi 1, 1 = Raspberry Pi > 1
+        oled = ssd1306(i2cbus)
+
+        # Ein paar Abkürzungen, um den Code zu entschlacken
+        draw = oled.canvas
+
+        # Display zum Start löschen
+        oled.cls()
+        oled.display()
+
+        # Hallo Welt
+        draw.text((1, 16), "TrainControll 2021", fill=1)
+        draw.text((1, 40), "", fill=1)
+
+        # Ausgaben auf Display schreiben
+        oled.display()
+
+
+if __name__ == "__main__":
+    init_i2c_display()
+    init_spi_display()
