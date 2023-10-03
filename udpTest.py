c12fb17544c3229185df6929cab988331a76deb6
MrFlexi <szarolettal@hotmail.com>
2023-10-03 20:55:42 +0100

    udp sniffer

--- udpTest.py
+++ udpTest.py
@@ -1,13 +1,27 @@
 import socket
+import sys
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005 
-UDP_Destination_port = 15730;
-UDP_Local_port = 15731;
+UDP_IP = "0.0.0.0"  #listening to all IP addresses
+UDP_Destination_port = 15730;  #  CS2 Script --> Python
+UDP_cs2_port = 15731;        #  Python --> CS2 Script
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((UDP_IP, UDP_Destination_port))
-    
-while True:
+
+
+try:
+    while True:
        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
        print("received message: %s" % data)
+       
+except KeyboardInterrupt:
+    pass
+    print('Finally block is executed')
+    sock.close( )
+
+
+       
+
+
+
